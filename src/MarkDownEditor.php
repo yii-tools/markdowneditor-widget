b81f69d2332aa66782fa5e0160607975359abe7d
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-02-05 12:59:38 -0300

    Initial commit. (#1)

---
+++ src/MarkDownEditor.php
@@ -0,0 +1,360 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yii\MarkDownEditor;
+
+use InvalidArgumentException;
+use JsonException;
+use Yii\FormModel\FormModelInterface;
+use Yii\SimpleMde\Asset\Npm\Min\MarkDownEditorAsset;
+use Yii\Widget\Attribute;
+use Yii\Widget\Input\AbstractInputWidget;
+use Yiisoft\Assets\AssetManager;
+use Yiisoft\Assets\Exception\InvalidConfigException;
+use Yiisoft\Strings\Inflector;
+use Yiisoft\View\WebView;
+
+use function array_key_exists;
+use function in_array;
+use function is_string;
+use function json_encode;
+
+final class MarkDownEditor extends AbstractInputWidget
+{
+    use Attribute\HasCols;
+    use Attribute\HasRows;
+    use Attribute\HasWrap;
+
+    /** @psalm-var array<string, mixed> $editorOptions */
+    private array $editorOptions = [];
+    private array $toolbar = [
+        'bold',
+        'italic',
+        'strikethrough',
+        'heading',
+        'heading-smaller',
+        'heading-bigger',
+        'heading-1',
+        'heading-2',
+        'heading-3',
+        'code',
+        'quote',
+        'unordered-list',
+        'ordered-list',
+        'link',
+        'image',
+        'table',
+        'horizontal-rule',
+        'preview',
+        'side-by-side',
+        'fullscreen',
+        'guide',
+    ];
+
+    public function __construct(
+        FormModelInterface $formModel,
+        string $attribute,
+        private readonly AssetManager $assetManager,
+        private readonly Webview $webView
+    ) {
+        parent::__construct($formModel, $attribute);
+    }
+
+    /**
+     * Returns a new instance specifying autofocuses the editor.
+     * Defaults to `false`.
+     */
+    public function autoFocusEditor(bool $value): self
+    {
+        $new = clone $this;
+        $new->editorOptions['autofocus'] = $value;
+
+        return $new;
+    }
+
+    /**
+     * Returns a new instance specifying saves the text that's being written and will load it back in the future.
+     * It will forget the text when the form it's contained in is submitted.
+     *
+     * @param int $delay The delay in milliseconds between each save.
+     * Defaults to `1000`.
+     */
+    public function autoSave(int $delay): self
+    {
+        $new = clone $this;
+        $new->editorOptions['autosave'] = [
+            'delay' => $delay,
+            'enabled' => true,
+            'uniqueId' => $this->getId(),
+        ];
+
+        return $new;
+    }
+
+    /**
+     * Returns a new instance specifying force text changes made in SimpleMDE to be immediately stored in original
+     * textarea.
+     * Defaults to `false`.
+     */
+    public function forceSync(bool $value): self
+    {
+        $new = clone $this;
+        $new->editorOptions['forceSync'] = $value;
+
+        return $new;
+    }
+
+    /**
+     * Returns a new instance specifying an array of icon names to hide. Can be used to hide specific icons shown by
+     * default without completely customizing the toolbar.
+     *
+     * @param array $icons The icon names to hide.
+     */
+    public function hiddenIcons(array $icons): self
+    {
+        $this->validateIconsToolbar($icons);
+
+        $new = clone $this;
+        $new->editorOptions['hideIcons'] = $icons;
+
+        return $new;
+    }
+
+    /**
+     * Returns a new instance specifying indent using spaces instead of tabs.
+     * Defaults to `true`.
+     */
+    public function indentWithTabs(bool $value): self
+    {
+        $new = clone $this;
+        $new->editorOptions['indentWithTabs'] = $value;
+
+        return $new;
+    }
+
+    /**
+     * Returns a new instance specifying the initial value of the editor.
+     *
+     * @param mixed $value The initial value of the editor.
+     */
+    public function initialValue(mixed $value): self
+    {
+        $new = clone $this;
+        $new->editorOptions['initialValue'] = $value;
+
+        return $new;
+    }
+
+    /**
+     * Returns a new instance specifying disable line wrapping.
+     * Defaults to `false`.
+     */
+    public function lineWrapping(bool $value): self
+    {
+        $new = clone $this;
+        $new->editorOptions['lineWrapping'] = $value;
+
+        return $new;
+    }
+
+    /**
+     * Returns a new instance specifying the options for the editor.
+     *
+     * @param string $attribute The name of the option.
+     * @param mixed $value The value of the option.
+     */
+    public function options(string $attribute, mixed $value): self
+    {
+        $new = clone $this;
+        $new->editorOptions[$attribute] = $value;
+
+        return $new;
+    }
+
+    /**
+     * Returns a new instance specifying the placeholder text to display when the editor is empty.
+     *
+     * @param string $value The placeholder text to display when the editor is empty.
+     */
+    public function placeholder(string $value): self
+    {
+        $new = clone $this;
+        $new->editorOptions['placeholder'] = $value;
+
+        return $new;
+    }
+
+    /**
+     * Returns a new instance that specifies whether a JS alert window requests the image URL or link.
+     */
+    public function promptURLs(bool $value): self
+    {
+        $new = clone $this;
+        $new->editorOptions['promptURLs'] = $value;
+
+        return $new;
+    }
+
+    /**
+     * @throws JsonException
+     * @throws InvalidConfigException
+     */
+    public function render(): string
+    {
+        $this->registerAssets();
+
+        return $this->renderTextArea();
+    }
+
+    /**
+     * Returns a new instance specifying the icons to show in the toolbar.
+     *
+     * @param array $icons The icon names to show.
+     */
+    public function showIcons(array $icons): self
+    {
+        $this->validateIconsToolbar($icons);
+
+        $new = clone $this;
+        $new->editorOptions['showIcons'] = $icons;
+
+        return $new;
+    }
+
+    /**
+     * Returns a new instance specifying whether spell checking is enabled.
+     * Defaults to `false`.
+     */
+    public function spellChecker(bool $value): self
+    {
+        $new = clone $this;
+        $new->editorOptions['spellChecker'] = $value;
+
+        return $new;
+    }
+
+    /**
+     * Returns a new instance specifying whether to style the selected text.
+     * Defaults to `false`.
+     */
+    public function styleSelectedText(bool $value): self
+    {
+        $new = clone $this;
+        $new->editorOptions['styleSelectedText'] = $value;
+
+        return $new;
+    }
+
+    /**
+     * Returns a new instance specifying the tab size.
+     * Defaults to `2`.
+     */
+    public function tabSize(int $value): self
+    {
+        $new = clone $this;
+        $new->editorOptions['tabSize'] = $value;
+
+        return $new;
+    }
+
+    /**
+     * Returns a new instance specifying the toolbar configuration.
+     *
+     * @param array $toolbar The toolbar configuration.
+     *
+     * @see toolbar
+     */
+    public function toolbar(array $toolbar): self
+    {
+        $this->validateIconsToolbar($toolbar);
+
+        $new = clone $this;
+        $new->editorOptions['toolbar'] = $toolbar;
+
+        return $new;
+    }
+
+    /**
+     * Returns a new instance specifying whether to show tooltips for toolbar buttons.
+     * Defaults to `false`.
+     *
+     * @param bool $value Whether to show tooltips for toolbar buttons.
+     */
+    public function toolbarTips(bool $value): self
+    {
+        $new = clone $this;
+        $new->editorOptions['toolbarTips'] = $value;
+
+        return $new;
+    }
+
+    /**
+     * @throws JsonException
+     */
+    private function getScript(): string
+    {
+        $config = '';
+        $editorOptions = $this->editorOptions;
+        $editorOptions['element'] = 'element: document.getElementById("' . $this->getId() . '"), ';
+
+        if (!isset($editorOptions['toolbar'])) {
+            $editorOptions['toolbar'] = $this->toolbar;
+        }
+
+        $varName = (new Inflector())->toPascalCase($this->getId());
+
+        /** @psalm-var mixed $value */
+        foreach ($editorOptions as $attribute => $value) {
+            $config .= match ($attribute) {
+                'element' => (string) $value,
+                default => $attribute . ': ' . json_encode($value, JSON_THROW_ON_ERROR),
+            };
+        }
+
+        return "var $varName = new SimpleMDE({ $config });";
+    }
+
+    /**
+     * @throws JsonException
+     * @throws InvalidConfigException
+     */
+    private function registerAssets(): void
+    {
+        $this->assetManager->register(MarkDownEditorAsset::class);
+        $this->webView->registerJs($this->getScript());
+    }
+
+    private function renderTextArea(): string
+    {
+        $attributes = $this->attributes;
+
+        $content = match (array_key_exists('value', $attributes)) {
+            true => $attributes['value'],
+            false => $this->getValue(),
+        };
+
+        unset($attributes['value']);
+
+        /**
+         * @link https://www.w3.org/TR/2012/WD-html-markup-20120329/syntax.html#contents
+         */
+        if (!is_string($content) && null !== $content) {
+            throw new InvalidArgumentException('TextArea widget must be a string or null value.');
+        }
+
+        $attributes['id'] = $this->getId();
+
+        return $this->run('textarea', (string) $content, null, $attributes);
+    }
+
+    private function validateIconsToolbar(array $icons): void
+    {
+        /** @psalm-var string[] $icons */
+        foreach ($icons as $icon) {
+            if (!in_array($icon, $this->toolbar, true)) {
+                throw new InvalidArgumentException('Invalid toolbar item: ' . $icon);
+            }
+        }
+    }
+}
---
+++ tests/ImmutabilityTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yii\MarkDownEditor\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Yii\MarkDownEditor\MarkDownEditor;
+use Yii\MarkDownEditor\Tests\Support\TestForm;
+use Yii\MarkDownEditor\Tests\Support\TestTrait;
+use Yiisoft\Definitions\Exception\CircularReferenceException;
+use Yiisoft\Definitions\Exception\InvalidConfigException;
+use Yiisoft\Definitions\Exception\NotInstantiableException;
+use Yiisoft\Factory\NotFoundException;
+
+/**
+ * @psalm-suppress PropertyNotSetInConstructor
+ */
+final class ImmutabilityTest extends TestCase
+{
+    use TestTrait;
+
+    /**
+     * @throws CircularReferenceException
+     * @throws InvalidConfigException
+     * @throws NotFoundException
+     * @throws NotInstantiableException
+     */
+    public function testImmutability(): void
+    {
+        $markDownEditor = MarkDownEditor::widget([new TestForm(), 'string']);
+
+        $this->assertNotSame($markDownEditor, $markDownEditor->autoFocusEditor(true));
+        $this->assertNotSame($markDownEditor, $markDownEditor->autoSave(1000));
+        $this->assertNotSame($markDownEditor, $markDownEditor->forceSync(true));
+        $this->assertNotSame($markDownEditor, $markDownEditor->hiddenIcons(['heading-1', 'heading-2', 'heading-3']));
+        $this->assertNotSame($markDownEditor, $markDownEditor->indentWithTabs(true));
+        $this->assertNotSame($markDownEditor, $markDownEditor->initialValue('initial value'));
+        $this->assertNotSame($markDownEditor, $markDownEditor->lineWrapping(false));
+        $this->assertNotSame($markDownEditor, $markDownEditor->options('autofocus', true));
+        $this->assertNotSame($markDownEditor, $markDownEditor->placeholder('placeholder'));
+        $this->assertNotSame($markDownEditor, $markDownEditor->promptURLs(false));
+        $this->assertNotSame($markDownEditor, $markDownEditor->showIcons(['heading-1', 'heading-2', 'heading-3']));
+        $this->assertNotSame($markDownEditor, $markDownEditor->spellChecker(false));
+        $this->assertNotSame($markDownEditor, $markDownEditor->styleSelectedText(false));
+        $this->assertNotSame($markDownEditor, $markDownEditor->tabSize(4));
+        $this->assertNotSame($markDownEditor, $markDownEditor->toolbar(['heading-1', 'heading-2', 'heading-3']));
+        $this->assertNotSame($markDownEditor, $markDownEditor->toolbarTips(false));
+    }
+}
